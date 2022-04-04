36ddb8ce1ca9e36c79ab868bc38354ae56d80ddb
Oliva7777RMZ <100888490+Oliva7777RMZ@users.noreply.github.com>
2022-04-04 14:51:03 -0500

    Add files via upload
    Ejercicio 4
    Codifique un programa que le pida al usuario su género y su estado civil. Dependiendo de ambos valores, el programa deberá decir: Soltera o Soltero, Casada o Casado, Divorciada o Divorciado, Viuda o Viudo. Si uno de los dos datos ingresados es incorrecto, se muestra cuál dato fue correcto y cuál fue no reconocido, o si ambos fueron no reconocidos.
    Nota: para este ejercicio, se recomienda utilizar un sólo scanf e ingresar los dos caracteres juntos, como se muestra a continuación:
    char genero, estadoCivil;
    printf("Ingresa tu genero y estado civil juntos sin espacios ");
    scanf("%c%c",&genero,&estadoCivil);

---
+++ main (30).c
@@ -0,0 +1,193 @@
+#include<stdio.h>
+int main(){
+    //Compilar
+    char genero, estadocivil;
+    //Pedir datos
+    printf("Ingresa tu genero y estado civil juntos sin espacios ");
+    scanf("%c%c",&genero,&estadocivil);
+    //Condicional
+   if(genero == 'm'){
+       if(estadocivil == 's'){
+            printf("Soltero ");
+        }
+        else{
+            if(estadocivil == 'S'){
+                printf("Soltero ");
+            }
+            else{
+                if(estadocivil == 'c'){
+                    printf("Casado ");
+                }
+                else{
+                    if(estadocivil == 'C'){
+                        printf("Casado ");
+                    }
+                    else{
+                        if(estadocivil == 'd'){
+                            printf("Divorciado ");
+                        }
+                        else{
+                            if(estadocivil == 'D'){
+                                printf("Divorciado ");
+                            }
+                            else{
+                                if(estadocivil == 'v'){
+                                    printf("Viudo");
+                                }
+                                else{
+                                    if(estadocivil == 'V'){
+                                        printf("Viudo ");
+                                    }
+                                    else{
+                                    	printf("Estado Invalido ");
+									}
+                                }
+                                
+                            
+                            }
+                        }
+                    }
+                }
+            }
+        }
+   }
+    if(genero == 'M'){
+       if(estadocivil == 's'){
+            printf("Soltero ");
+        }
+        else{
+            if(estadocivil == 'S'){
+                printf("Soltero ");
+            }
+            else{
+                if(estadocivil == 'c'){
+                    printf("Casado ");
+                }
+                else{
+                    if(estadocivil == 'C'){
+                        printf("Casado ");
+                    }
+                    else{
+                        if(estadocivil == 'd'){
+                            printf("Divorciado ");
+                        }
+                        else{
+                            if(estadocivil == 'D'){
+                                printf("Divorciado ");
+                            }
+                            else{
+                                if(estadocivil == 'v'){
+                                    printf("Viudo");
+                                }
+                                else{
+                                    if(estadocivil == 'V'){
+                                        printf("Viudo ");
+                                    }
+                                    else{
+                                    	printf("Estado Invalido ");
+									}
+                                }
+                                
+                            
+                            }
+                        }
+                    }
+                }
+            }
+        }
+   }
+    if(genero == 'f'){
+       if(estadocivil == 's'){
+            printf("Soltera ");
+        }
+        else{
+            if(estadocivil == 'S'){
+                printf("Soltera ");
+            }
+            else{
+                if(estadocivil == 'c'){
+                    printf("Casada ");
+                }
+                else{
+                    if(estadocivil == 'C'){
+                        printf("Casada ");
+                    }
+                    else{
+                        if(estadocivil == 'd'){
+                            printf("Divorciada ");
+                        }
+                        else{
+                            if(estadocivil == 'D'){
+                                printf("Divorciada ");
+                            }
+                            else{
+                                if(estadocivil == 'v'){
+                                    printf("Viuda ");
+                                }
+                                else{
+                                    if(estadocivil == 'V'){
+                                        printf("Viuda ");
+                                    }
+                                    else{
+                                    	printf("Estado Invalido ");
+									}
+                                }
+                                
+                            
+                            }
+                        }
+                    }
+                }
+            }
+        }
+   }
+    if(genero == 'F'){
+       if(estadocivil == 's'){
+            printf("Soltera ");
+        }
+        else{
+            if(estadocivil == 'S'){
+                printf("Soltera ");
+            }
+            else{
+                if(estadocivil == 'c'){
+                    printf("Casada ");
+                }
+                else{
+                    if(estadocivil == 'C'){
+                        printf("Casada ");
+                    }
+                    else{
+                        if(estadocivil == 'd'){
+                            printf("Divorciada ");
+                        }
+                        else{
+                            if(estadocivil == 'D'){
+                                printf("Divorciada ");
+                            }
+                            else{
+                                if(estadocivil == 'v'){
+                                    printf("Viuda ");
+                                }
+                                else{
+                                    if(estadocivil == 'V'){
+                                        printf("Viuda ");
+                                    }
+                                    else{
+                                    	printf("Estado Invalido ");
+									}
+                                }
+                                
+                            
+                            }
+                        }
+                    }
+                }
+            }
+        }
+   }
+else{
+printf("Genero Invalido ");
+  }
+    return 0;
+}
